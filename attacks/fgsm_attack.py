028e8f4528afab4dc97ed3ec893bd7ccb4c45b5b
Heather <heathermariehuggins@gmail.com>
2024-09-17 12:39:13 -0700

    just some code comments

--- attacks/fgsm_attack.py
+++ attacks/fgsm_attack.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 def fgsm_attack(model, images, labels, epsilon):
+    # Get gradients of loss, re: input image
     loss_object = tf.keras.losses.CategoricalCrossentropy()
     with tf.GradientTape() as tape:
         tape.watch(images)
@@ -9,6 +10,7 @@ def fgsm_attack(model, images, labels, epsilon):
 
     gradient = tape.gradient(loss, images)
 
+    #Get sign of gradients to create perturbation
     signed_grad = tf.sign(gradient)
     adversarial_images = images + epsilon * signed_grad
     return tf.clip_by_value(adversarial_images, 0, 1)
